480a8265829585d760a9eacd4ac0970cd317bb0e
Ari Munandar <arimunandar.dev@gmail.com>
2020-03-26 01:28:40 +0700

    Remove SPM

--- Example/Pods/Target Support Files/ARTDEVRouter/ARTDEVRouter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ARTDEVRouter : NSObject
-@end
-@implementation PodsDummy_ARTDEVRouter
-@end
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import ARTDEVRouterTests
-
-var tests = [XCTestCaseEntry]()
-tests += ARTDEVRouterTests.allTests()
-XCTMain(tests)
